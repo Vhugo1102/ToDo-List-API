dd2bd71bdee88e87618b7a9e9105d5cb0d7439bc
Vitor Hugo da Silva <149020824+Vhugo1102@users.noreply.github.com>
2025-04-21 20:19:03 -0300

    Estrutura inicial do projeto ToDo-List-API

---
+++ src/test/java/br/com/vhs/todo_list_api/TodoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vhs.todo_list_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
